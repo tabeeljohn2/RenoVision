fb60b0a1c43cc710e7724bd8d8a18bc732610f3d
Tabeel John <tabeeljohn2@gmail.com>
2026-07-22 17:28:12 +0500

    Add files via upload

---
+++ Frontend/renovision-ui/public/sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'renovision-v1';
+
+const CACHED_URLS = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(CACHED_URLS);
+    }).catch((err) => {
+      console.log('Cache error:', err);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
